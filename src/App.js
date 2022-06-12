68af26b9200169d5464ca563ce059b073fc49489
haagmin5382 <itworker5382@gmail.com>
2022-06-12 23:13:29 +0900

    feat : emotion.js basic

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import "./App.css";
+/** @jsxImportSource @emotion/react */
+import { jsx, css } from "@emotion/react";
+
+const spanStyle = css`
+  background-color: gray;
+  color: white;
+  font-size: 2vw;
+  // emotion.js는 styled-components처럼 브라우저에서 열었을 때 className을 임의로 생성해준다.
+  // @emotion/babel-plugin을 사용하면 커스터마이징 가능하다.
+  label: spanStyle;
+`;
 
 function App() {
-  return <div>시작합니다</div>;
+  return (
+    <div>
+      <span css={spanStyle}>hello world</span>
+    </div>
+  );
 }
 
 export default App;
